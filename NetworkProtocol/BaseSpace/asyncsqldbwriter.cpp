0c1e9f91bb7f924c38a90b84b609ecbd7c0e5060
EndrII <EndrIIMail@gmail.com>
2020-08-02 13:36:20 +0300

    set thread save of AsyncSqlDbWriter

--- NetworkProtocol/BaseSpace/asyncsqldbwriter.cpp
+++ NetworkProtocol/BaseSpace/asyncsqldbwriter.cpp
@@ -7,6 +7,7 @@
 
 #include "asyncsqldbwriter.h"
 
+#include <QDateTime>
 #include <QMetaMethod>
 #include <QThread>
 #include <dbobject.h>
@@ -20,37 +21,94 @@ AsyncSqlDbWriter::AsyncSqlDbWriter(QObject *ptr):
 {
     QThread *own = new QThread(this);
     moveToThread(own);
+    _mainThreadId = QThread::currentThreadId();
+
 }
 
 bool AsyncSqlDbWriter::saveObject(const DBObject *saveObject) {
+    if (QThread::currentThreadId() == _mainThreadId) {
+        return SqlDBWriter::saveObject(saveObject);
+    }
+
     return QMetaObject::invokeMethod(this,
-                              "handleSaveObject",
-                              Qt::QueuedConnection,
-                              Q_ARG(const DBObject *,
-                                    saveObject));
+                                     "handleSaveObject",
+                                     Qt::QueuedConnection,
+                                     Q_ARG(const DBObject *,
+                                           saveObject));
 
 }
 
 bool AsyncSqlDbWriter::deleteObject(const DBObject *deleteObject) {
+    if (QThread::currentThreadId() == _mainThreadId) {
+        return SqlDBWriter::deleteObject(deleteObject);
+    }
+
     return QMetaObject::invokeMethod(this,
-                               "handleDeleteObject",
-                               Qt::QueuedConnection,
-                               Q_ARG(const DBObject *,
-                                     deleteObject));
+                                     "handleDeleteObject",
+                                     Qt::QueuedConnection,
+                                     Q_ARG(const DBObject *,
+                                           deleteObject));
+}
+
+bool AsyncSqlDbWriter::getAllObjects(const DBObject &templateObject, QList<DBObject *> &result) {
+
+    if (QThread::currentThreadId() == _mainThreadId) {
+        return SqlDBWriter::getAllObjects(templateObject, result);
+    }
+
+    bool workOfEnd = false, workResult = false;
+    bool invockeResult = QMetaObject::invokeMethod(this,
+                                                   "handleDeleteObject",
+                                                   Qt::QueuedConnection,
+                                                   Q_ARG(const DBObject &, templateObject),
+                                                   Q_ARG(QList<DBObject *> &, result),
+                                                   Q_ARG(bool *, &workResult),
+                                                   Q_ARG(bool *, &workOfEnd));
+
+    if (!invockeResult)
+        return false;
+
+
+    if (!waitFor(&workOfEnd)) {
+        return false;
+    }
+
+    return workResult;
 }
 
 void AsyncSqlDbWriter::handleSaveObject(const DBObject* saveObject) {
     if (!SqlDBWriter::saveObject(saveObject)) {
         QuasarAppUtils::Params::log("AsyncSqlDbWriter: save object fail!",
-                                           QuasarAppUtils::Error);
+                                    QuasarAppUtils::Error);
     }
 }
 
 void AsyncSqlDbWriter::handleDeleteObject(const DBObject *deleteObject) {
     if (!SqlDBWriter::deleteObject(deleteObject)) {
         QuasarAppUtils::Params::log("AsyncSqlDbWriter: delete object fail!",
-                                           QuasarAppUtils::Error);
+                                    QuasarAppUtils::Error);
     }
+}
+
+void AsyncSqlDbWriter::handleGetAllObject(const DBObject &templateObject, QList<DBObject *> &result,
+                                          bool *resultOfWork, bool *endOfWork) {
+
+
+    *resultOfWork = SqlDBWriter::getAllObjects(templateObject, result);
+
+    if (endOfWork) {
+        *endOfWork = true;
+    }
+
+}
+
+bool AsyncSqlDbWriter::waitFor(bool *condition, int timeout) const {
+    auto curmsec = QDateTime::currentMSecsSinceEpoch() + timeout;
+    while (curmsec > QDateTime::currentMSecsSinceEpoch() && *condition) {
+        QCoreApplication::processEvents();
+    }
+    QCoreApplication::processEvents();
+    return *condition;
 }
 
 }
--- NetworkProtocol/BaseSpace/asyncsqldbwriter.h
+++ NetworkProtocol/BaseSpace/asyncsqldbwriter.h
@@ -14,7 +14,8 @@ namespace NP {
 
 
 /**
- * @brief The AsyncSqlDbWriter class some as SqlDBWriter bud run all commnad in own thread
+ * @brief The AsyncSqlDbWriter class some as SqlDBWriter bud run all commnad in main thread
+ * is thread save db writer.
  */
 class AsyncSqlDbWriter :public QObject, public SqlDBWriter
 {
@@ -25,10 +26,27 @@ public:
     // iObjectProvider interface
     bool saveObject(const DBObject* saveObject) override;
     bool deleteObject(const DBObject* deleteObject) override;
+    bool getAllObjects(const DBObject &templateObject, QList<DBObject *> &result) override;
 
-private slots:
+protected slots:
     void handleSaveObject(const DBObject* saveObject);
     void handleDeleteObject(const DBObject* deleteObject);
+
+    /**
+     * @brief the handleGetAllObject - this method call getAllObjects on main thread.
+     * @arg templateObject - the some as in getAllObjects
+     * @arg result - the some as in getAllObjects
+     * @arg resultOfWork - this ptr contains result of invoked of getAllObjects method on main thread
+     * @arg endOfWork - this ptr set true when invocked method is finished
+     * @arg cb - this call back method invoke after getAllObjects method
+     */
+    virtual void handleGetAllObject(const DBObject &templateObject, QList<DBObject *> &result,
+                                    bool *resultOfWork, bool *endOfWork = nullptr);
+
+private:
+    bool waitFor(bool* condition, int timeout = 30000) const;
+
+    Qt::HANDLE _mainThreadId;
 
 };
 
