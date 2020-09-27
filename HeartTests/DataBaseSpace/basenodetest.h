f6e010a0089fded73b1909a9ed2dc12f949818a7
EndrII <EndrIIMail@gmail.com>
2020-09-27 12:40:11 +0300

    added sign for sources

--- HeartTests/DataBaseSpace/basenodetest.h
+++ HeartTests/DataBaseSpace/basenodetest.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2018-2020 QuasarApp.
+ * Distributed under the lgplv3 software license, see the accompanying
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+*/
+
+
 #ifndef BASENODETEST_H
 #define BASENODETEST_H
 #include "basetestutils.h"
--- HeartTests/DataBaseSpace/basenodeunittests.cpp
+++ HeartTests/DataBaseSpace/basenodeunittests.cpp
@@ -1,4 +1,12 @@
 #include "basenodeunittests.h"
+/*
+ * Copyright (C) 2018-2020 QuasarApp.
+ * Distributed under the lgplv3 software license, see the accompanying
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+*/
+
+
 #include "test.h"
 #include "sqldbcache.h"
 #include <QFileInfo>
