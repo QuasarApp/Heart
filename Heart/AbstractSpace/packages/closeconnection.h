236a4655f1e42af5410580bf08ebee87adab6c4d
Oleg-designer <sweetletal0@gmail.com>
2021-03-10 10:19:58 +0300

    ref #19 closeconnection.h grammar correction

--- Heart/AbstractSpace/packages/closeconnection.h
+++ Heart/AbstractSpace/packages/closeconnection.h
@@ -16,10 +16,10 @@ namespace QH{
 namespace PKG {
 
 /**
- * @brief The CloseConnection class - This commnad is request for close connection on parent node of connection
- * This neede becouse QAbstractSocket emit error when connection closed from remote node.
+ * @brief The CloseConnection class - This commanad is request for close connection on parent node of connection.
+ * This need becouse QAbstractSocket emit error when connection closed from remote node.
  *
- * From QtDocumentation :
+ * From QtDocumentation:
  *
  *
  *  If the remote host closes the connection, QAbstractSocket will emit errorOccurred(QAbstractSocket::RemoteHostClosedError), during which the socket state will still be ConnectedState, and then the disconnected() signal will be emitted.
