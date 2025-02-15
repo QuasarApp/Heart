d232c76da209e69893279423c4147a1bbf757d3c
EndrII <EndrIIMail@gmail.com>
2025-02-15 16:56:48 +0100

    added plugins classes

---
+++ src/public/plugin.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2025-2025 QuasarApp.
+ * Distributed under the lgplv3 software license, see the accompanying
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+*/
+
+#ifndef PLUGIN_H
+#define PLUGIN_H
+
+namespace QH {
+
+class Plugin
+{
+public:
+    Plugin();
+};
+}
+#endif // PLUGIN_H
---
+++ src/public/pluginloader.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2025-2025 QuasarApp.
+ * Distributed under the lgplv3 software license, see the accompanying
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+*/
+
+#ifndef PLUGINLOADER_H
+#define PLUGINLOADER_H
+namespace QH {
+
+class PluginLoader
+{
+public:
+    PluginLoader();
+};
+}
+#endif // PLUGINLOADER_H
